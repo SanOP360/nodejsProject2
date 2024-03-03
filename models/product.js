1fc63d4e460a3da670ec8f197028b6e1ddb6d277
SanOP360 <sanjayl2001alm@gmail.com>
2024-03-03 12:40:02 +0530

    added models

---
+++ models/product.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+const path = require("path");
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "products.json"
+);
+
+const getProductsFile = (callback) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      callback([]);
+    } else {
+      callback(JSON.parse(fileContent));
+    }
+  });
+};
+
+module.exports = class Product {
+  constructor(title) {
+    this.title = title;
+  }
+
+  save() {
+    getProductsFile((products) => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(callback) {
+    getProductsFile(callback);
+  }
+};
